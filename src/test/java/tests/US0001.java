0e87e8acbfc31da1d2f821e755acec13363bfb58
Alikan <dralikan94@gmail.com>
2022-06-23 00:14:54 +0300

    us0001 classi eklendi

---
+++ src/test/java/tests/US0001.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.TradylinnPage;
+
+public class US0001 {
+
+    TradylinnPage tradylinnPage = new TradylinnPage();
+
+    @BeforeClass
+    public void registerSayfayaGider_Ali(){
+
+    }
+}
