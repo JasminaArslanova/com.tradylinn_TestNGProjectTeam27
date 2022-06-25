97e252440f05a935052bb3ef4341c5cd9780e379
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-06-25 23:56:37 +0300

    25.06_US008

--- src/test/java/pages/TradylinnPage.java
+++ src/test/java/pages/TradylinnPage.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
-import java.util.Arrays;
 import java.util.List;
 public class TradylinnPage {
 public TradylinnPage(){PageFactory.initElements(Driver.getDriver(),this);}
