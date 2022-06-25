0cdacadcbc71ee81c46158b0ea17f665fabf0e05
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-06-26 00:31:28 +0300

    26.06_US008

--- src/test/java/tests/US008.java
+++ src/test/java/tests/US008.java
@@ -1,9 +1,15 @@
 package tests;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.TradylinnPage;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static tests.US007.signInMethod;
+import static utilities.ReusableMethods.getElementsText;
 
 
 public class US008 {
@@ -23,6 +29,8 @@ public class US008 {
         tradylinPage.manageStockCheckbox.click();
         softAssert.assertTrue(tradylinPage.manageStockCheckbox.isSelected(),
                 "\"Manage Stock\" checkbox is not selected");
+
+        softAssert.assertAll();
     }
 
     @Test (dependsOnMethods = "TC001")
@@ -35,23 +43,24 @@ public class US008 {
                 "\"Stock Qty\" textbox is not enabled\"");
 
         tradylinPage.stockQtytextbox.sendKeys("10");
+
+        softAssert.assertAll();
     }
 
     @Test(dependsOnMethods = "TC001")
     public void TC003() {
         Select select=new Select(tradylinPage.backordersDropdown);
 
-        softAssert.assertTrue(tradylinPage.allowBackordersHead.getText().contains("Allow Backorders"),
-                "Head does not contain \"Allow Backorders\"");
-        softAssert.assertTrue(tradylinPage.backordersDropdown.getText().contains("Do not Allow"),
-                "\"Do not Allow\" option is not founded");
-        softAssert.assertTrue(tradylinPage.backordersDropdown.getText().contains("Allow, but notify customer"),
-                "\"Allow, but notify customer\" option is not founded");
-        softAssert.assertTrue(tradylinPage.backordersDropdown.getText().contains("Allow"),
-                "\"Allow\" option is not founded");
+        List<WebElement> actualOptionList=select.getOptions();
+        List<String> optionList=getElementsText(actualOptionList);
+        List<String> expectedOptionList= Arrays.asList("Do not Allow","Allow, but notify customer","Allow");
 
-        select.selectByVisibleText("Do not Allow");
-        softAssert.assertTrue(select.getFirstSelectedOption().isSelected(),"\"Do not Allow\" option is not selected");
-
+        for (String each:expectedOptionList) {
+            softAssert.assertTrue(optionList.contains(each),"Option List does not contain "+each+" ");
+            select.selectByVisibleText(each);
+            softAssert.assertTrue(select.getFirstSelectedOption().getText().contains(each),
+                    " "+each+" option is not selected");
+        }
+        softAssert.assertAll();
     }
 }
