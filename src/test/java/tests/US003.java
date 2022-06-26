b8c3e2442390d3b82f78e64767d35bc38ef2ec23
Jasmina Arslanova <zhasmina.arslanova@gmail.com>
2022-06-26 19:04:43 +0300

    26.06_US007/US008

--- src/test/java/tests/US003.java
+++ src/test/java/tests/US003.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
