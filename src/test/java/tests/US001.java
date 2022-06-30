33bed24e756bc8bd5ba5b4f23fd4b663141da1eb
Alikan <dralikan94@gmail.com>
2022-06-30 22:31:51 +0300

    us01 ilk4tc'ye rapor eklendi

--- src/test/java/tests/US001.java
+++ src/test/java/tests/US001.java
@@ -5,18 +5,21 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.TradylinnPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class US001 {
+public class US001 extends TestBaseRapor {
 
     static TradylinnPage tradylinnPage = new TradylinnPage();
     static Actions actions = new Actions(Driver.getDriver());
+    SoftAssert softAssert = new SoftAssert();
     static JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
     static Faker faker = new Faker();
     static int beklemeSure = 1;
@@ -54,20 +57,27 @@ public class US001 {
     public void testCase001() {
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
         ReusableMethods.waitFor(beklemeSure);
+        extentTest=extentReports.createTest("TC001_VendorCanSeeRegistrationPage",
+                "Vendor olarak, registration sayfasına gidebilmeli ve sayfadaki email, password, confirm password gorebilmeli");
+
 
         js.executeScript("arguments[0].click();", tradylinnPage.uyeOlHeaderButton);
+        extentTest.info("Tradylinn uye ol ekranina gidildi");
         // tradylinnPage.uyeOlHeaderButton.click();
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
+        softAssert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
+        extentTest.pass("Uye ol pop-up is displayed");
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        softAssert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.pass("Register as Vendor link is enabled");
 
         //      "5_sign up as a vendor'a tıklar ve regist. sayfasına gider
         tradylinnPage.registerAsVendorLinkText.click();
         //      Registration sayfasında oldugunu dogrular"
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.registerForm);
-        Assert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        extentTest.pass("Vendor Registration sayfasina gidildi");
         //      "6_Registration sayfasında email yazdigini ve
         //      email textboxun erişilebilir oldugunu kontrol eder"
         //      "7_Registration sayfasında password yazdigini ve
@@ -85,131 +95,182 @@ public class US001 {
         ReusableMethods.waitFor(beklemeSure);
         String actualConfirmPwText = tradylinnPage.vendorRegisterConfirmPwText.getText().replace("*", "");
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertEquals(actualEmailText, expectedEmailText);
-        Assert.assertEquals(expectedPasswordText, actualPasswordText);
-        Assert.assertEquals(expectedConfirmPwText, actualConfirmPwText);
+        softAssert.assertEquals(actualEmailText, expectedEmailText);
+        extentTest.pass("Email text is correct");
+        softAssert.assertEquals(expectedPasswordText, actualPasswordText);
+        extentTest.pass("Password text is correct");
+        softAssert.assertEquals(expectedConfirmPwText, actualConfirmPwText);
+        extentTest.pass("Confirm Password text is correct");
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.vendorRegisterEmailTextBox.isEnabled());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterEmailTextBox.isEnabled());
+        extentTest.pass("Email text box is enabled.");
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.vendorRegisterPasswordTextBox.isEnabled());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterPasswordTextBox.isEnabled());
+        extentTest.pass("Password text box is enabled.");
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.vendorRegisterConfirmPwTextBox.isEnabled());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterConfirmPwTextBox.isEnabled());
+        extentTest.pass("Confirm Password text is enabled.");
 
-        //Driver.closeDriver();
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
     }
 
     @Test(priority = 1)
     public void testCase002() {
+        extentTest=extentReports.createTest("TC002_VendorCanRegisterWithCorrectDatas",
+                "Vendor olarak, unique, gecerli, gulch bir password girilmeli ve password dogru onaylanmalı üyelik gerceklesmeli");
+
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
         ReusableMethods.waitFor(beklemeSure);
 
         tradylinnPage.uyeOlHeaderButton.click();
-        Assert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
-        Assert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
-
+        extentTest.info("Tradylinn uye ol ekranina gidildi");
+        softAssert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
+        extentTest.pass("Uye ol pop-up is displayed");
+        softAssert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.pass("Register as Vendor link is enabled");
         //      "5_sign up as a vendor'a tıklar ve regist. sayfasına gider
         tradylinnPage.registerAsVendorLinkText.click();
-
+        extentTest.info("Vendor Registration sayfasina gidildi");
         //      Registration sayfasında oldugunu dogrular"
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.registerForm);
-        Assert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        extentTest.pass("Vendor Registration sayfasina gidildi");
         //4_gecerli email girer
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterEmailTextBox.sendKeys(faker.internet().emailAddress());
+        extentTest.info("Entered unique email adress.");
         //"5_gulch password girer ve strong mesajı çıktığını görür"
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterPasswordTextBox.sendKeys(ConfigReader.getProperty("gulchPassword"));
+        extentTest.info("Entered a gulch password.");
         //6_passwordu dogru onaylar
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterConfirmPwTextBox.sendKeys(ConfigReader.getProperty("gulchPassword"));
+        extentTest.info("Password confirmed correctly.");
         //7_register butonuna basar
         js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.vendorRegisterPageButton);
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].click();", tradylinnPage.vendorRegisterPageButton);
+        extentTest.info("Register button clicked.");
         //tradylinnPage.vendorRegisterPageButton.click();
         //8_onay yazısının çıktğını doğrular
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.registrationSuccessHeader.isDisplayed());
+        softAssert.assertTrue(tradylinnPage.registrationSuccessHeader.isDisplayed());
+        extentTest.pass("Registered as vendor.");
 
-        //Driver.closeDriver();
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
     }
 
     @Test(priority = 2)
     public void testCase003() {
+
+        extentTest=extentReports.createTest("TC003_VendorCanNotRegisterWithUsedEmail",
+                "Vendor olarak, unique olmayan bir email ile uyelik gerceklesmemeli");
+
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
         ReusableMethods.waitFor(beklemeSure);
 
         tradylinnPage.uyeOlHeaderButton.click();
-        Assert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
-        Assert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.info("Tradylinn uye ol ekranina gidildi");
+
+        softAssert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
+        extentTest.pass("Uye ol pop-up is displayed");
+
+        softAssert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.pass("Register as Vendor link is enabled");
 
         //      "5_sign up as a vendor'a tıklar ve regist. sayfasına gider
         tradylinnPage.registerAsVendorLinkText.click();
+        extentTest.info("Vendor Registration sayfasina gidildi");
         //      Registration sayfasında oldugunu dogrular"
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.registerForm);
-        Assert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        softAssert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        extentTest.pass("Vendor Registration sayfasina gidildi");
         // 4_unique olmayan email girer
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterEmailTextBox.sendKeys(ConfigReader.getProperty("usermail"));
+        extentTest.info("Entered no-unique email adress.");
         // "5_gulch password girer ve strong mesajı çıktığını görür"
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterPasswordTextBox.sendKeys(ConfigReader.getProperty("gulchPassword"));
+        extentTest.info("Entered a gulch password.");
         // 6_passwordu dogru onaylar
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterConfirmPwTextBox.sendKeys(ConfigReader.getProperty("gulchPassword"));
+        extentTest.info("Password confirmed correctly.");
         // 7_register butonuna basar
         //js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.vendorRegisterPageButton);
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].click();", tradylinnPage.vendorRegisterPageButton);
+        extentTest.info("Register button clicked.");
         // 8_"already exist" uyarısının çıktğını doğrular
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
-        Assert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("registeredVendorExistMessage"));
-
-        //Driver.closeDriver();
+        softAssert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
+        softAssert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("registeredVendorExistMessage"));
+        extentTest.pass("Not registered as vendor and 'already exist' message is visible.");
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
     }
 
     @Test(priority = 3)
     public void testCase004() {
+
+        extentTest=extentReports.createTest("TC004_VendorCanSeeRegistrationPage",
+                "Vendor olarak, unique, gecerli, gulch bir password girilmeli ve password dogru onaylanmadan üyelik gerceklesmemeli.");
+
         Driver.getDriver().get(ConfigReader.getProperty("tradylinnUrl"));
         ReusableMethods.waitFor(beklemeSure);
 
         tradylinnPage.uyeOlHeaderButton.click();
-        Assert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
-        Assert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.info("Tradylinn uye ol ekranina gidildi");
+        softAssert.assertTrue(tradylinnPage.kayitOlPopUpButton.isDisplayed());
+        extentTest.pass("Uye ol pop-up is displayed");
+        softAssert.assertTrue(tradylinnPage.registerAsVendorLinkText.isEnabled());
+        extentTest.pass("Register as Vendor link is enabled");
 
         //      "5_sign up as a vendor'a tıklar ve regist. sayfasına gider
         tradylinnPage.registerAsVendorLinkText.click();
+        extentTest.info("Vendor Registration sayfasina gidildi");
         //      Registration sayfasında oldugunu dogrular"
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.registerForm);
-        Assert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
+        extentTest.pass("Vendor Registration sayfasina gidildi");
+        softAssert.assertTrue(tradylinnPage.vendorRegisterBaslik.isDisplayed());
         ReusableMethods.waitFor(beklemeSure);
+        // 4_unique email girer
         tradylinnPage.vendorRegisterEmailTextBox.sendKeys(faker.internet().emailAddress());
+        extentTest.info("Entered unique email adress.");
         // "5_gulch password girer ve strong mesajı çıktığını görür"
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterPasswordTextBox.sendKeys(ConfigReader.getProperty("gulchPassword"));
+        extentTest.info("Entered a gulch password.");
+
         // 6_passwordu YANLIS onaylar
         ReusableMethods.waitFor(beklemeSure);
         tradylinnPage.vendorRegisterConfirmPwTextBox.sendKeys(ConfigReader.getProperty("shortPassword"));
+        extentTest.info("Password confirmed incorrectly.");
         // 7_register butonuna basar
         //js.executeScript("arguments[0].scrollIntoView()", tradylinnPage.vendorRegisterPageButton);
         ReusableMethods.waitFor(beklemeSure);
         js.executeScript("arguments[0].click();", tradylinnPage.vendorRegisterPageButton);
+        extentTest.info("Register button clicked.");
         //"8_""Password and Confirm-password are not same.""
         ReusableMethods.waitFor(beklemeSure);
-        Assert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
-        Assert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("wrongConfirmedPasswordMessage"));
-
-        //Driver.closeDriver();
+        softAssert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
+        softAssert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("wrongConfirmedPasswordMessage"));
+        extentTest.pass("Not registered as vendor and 'Password and Confirm-password are not same.' message is visible.");
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
     }
@@ -327,7 +388,7 @@ public class US001 {
         js.executeScript("arguments[0].click();", tradylinnPage.vendorRegisterPageButton);
         //"7_""Lütfen geçerli bir e-posta adresi verin.""
         //uyarısının çıktğını doğrular"
-        ReusableMethods.waitFor(beklemeSure);
+        ReusableMethods.waitFor(beklemeSure+2);
         Assert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
         Assert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("registeredVendorGecersizEmailMessage"));
 
@@ -347,7 +408,7 @@ public class US001 {
         js.executeScript("arguments[0].click();", tradylinnPage.vendorRegisterPageButton);
         //"11_""Lütfen geçerli bir e-posta adresi verin.""
         //uyarısının çıktğını doğrular"
-        ReusableMethods.waitFor(beklemeSure);
+        ReusableMethods.waitFor(beklemeSure+2);
         Assert.assertTrue(tradylinnPage.registrationCancelledMessage.isDisplayed());
         Assert.assertEquals(tradylinnPage.registrationCancelledMessage.getText(), ConfigReader.getProperty("registeredVendorGecersizEmailMessage"));
 
